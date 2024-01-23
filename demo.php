b9c889f72d3d0038b1be4101d10c64b91ca94af9
unknown <dustinmarinay0828@gmail.com>
2024-01-22 18:33:10 -0800

    Newfile

---
+++ demo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<body>
+    <form action="form_1.php" method="post">
+        Firstname:
+        <input type="text" name="firstname" required/>
+        <br>
+        <br>
+        LastName
+        <input> type="text" name="lastname" <required>
+            <br>
+            <br>
+            Adress
+            <input type="text name="address" required/>
+            <br>
+            <br>
+            Email Adress:
+            <input type="email" required/>
+            <br>
+            <br>
+            Password:
+            <input type="password" name="password" required/>
+            <br>
+            <br>
+            <input type="submit" value=Submit>
+</form>
+</body>
+</html>
